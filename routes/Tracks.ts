d878614908cf553507e54b265403a585e8e1df20
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-12 17:40:10 +0600

    Написал логику получение всех треков и получение треков по параметру

--- routes/Tracks.ts
+++ routes/Tracks.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import Track from "../models/Track";
 
 const tracksRouter = express.Router();
 
 tracksRouter.get('/', async (req, res) => {
-  res.send('get');
+  const query = req.query.album as string;
+
+  try {
+    const tracks = await Track.find().populate('album');
+
+    if (req.query.album !== undefined) {
+      const tracksId = await Track.find({album: query}).populate('album');
+      return res.send(tracksId);
+    } else {
+      return res.send(tracks);
+    }
+  }catch (e) {
+    return res.sendStatus(500)
+  }
 });
 
 tracksRouter.post('/', async (req, res) => {
