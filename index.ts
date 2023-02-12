02ba18465b7153aa3ab77cf33676c49db44e1fec
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-12 15:35:56 +0600

    Подключил роутеры к index

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 import express from 'express'
 import mongoose from "mongoose";
+import artistRouter from "./routes/artist";
+import albumsRouter from "./routes/albums";
+import tracksRouter from "./routes/Tracks";
 
 const app = express();
 const port = 8000;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/artists', artistRouter);
+app.use('/albums', albumsRouter);
+app.use('/tracks', tracksRouter);
 
 const run = async () => {
   mongoose.set('strictQuery', false);
