6218fdf758bb8daa7bb53450fcd528abfa3f4a16
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-12 17:22:03 +0600

    Написал логику получения всех альбомов также получение по id

--- routes/albums.ts
+++ routes/albums.ts
@@ -1,16 +1,35 @@
 import express from "express";
+import Album from "../models/Album";
 
 const albumsRouter = express.Router();
 
 albumsRouter.get('/', async (req, res) => {
-  res.send('get');
+  const query = req.query.artist as string;
+
+  try {
+    const albums = await Album.find().populate('executor');
+
+    if (req.query.artist !== undefined) {
+      const albums = await Album.find({executor: query});
+      return res.send(albums);
+    } else {
+      return res.send(albums);
+    }
+  }catch {
+    return res.sendStatus(500)
+  }
 });
 
 albumsRouter.get('/:id', async (req, res) => {
-  res.send('get id');
+  try {
+    const album = await Album.findById(req.params.id).populate('executor');
+    return res.send(album)
+  }catch {
+    return res.send(500)
+  }
 });
 
-albumsRouter.post('/', async (req, res) => {
+albumsRouter.post('/',async (req, res, next) => {
   res.send('post');
 });
 
