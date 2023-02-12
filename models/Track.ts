a0476d4cb5040afc46587519d872753dbec1312b
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-12 15:14:16 +0600

    Написал схему для треков

---
+++ models/Track.ts
@@ -0,0 +1,24 @@
+import mongoose, {Types} from "mongoose";
+import Album from "./Album";
+
+const Schema = mongoose.Schema;
+
+const TrackSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  album: {
+    type: Schema.Types.ObjectId,
+    ref: 'Album',
+    validate: {
+      validator: async (value: Types.ObjectId) => Album.findById(value),
+      message: 'Artist does not exist'
+    }
+  },
+  duration: String
+});
+
+const Track = mongoose.model('tracks', TrackSchema);
+
+export default Track
