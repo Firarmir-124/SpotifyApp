683e25850065ba84bd62b7153f7ff5c9c70f0c90
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-12 17:47:08 +0600

    Написал логику создание трека

--- routes/Tracks.ts
+++ routes/Tracks.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import Track from "../models/Track";
+import {TrackMutation} from "../types";
+import mongoose from "mongoose";
 
 const tracksRouter = express.Router();
 
@@ -20,8 +22,25 @@ tracksRouter.get('/', async (req, res) => {
   }
 });
 
-tracksRouter.post('/', async (req, res) => {
-  res.send('post');
+tracksRouter.post('/', async (req, res, next) => {
+  const newTrack:TrackMutation = {
+    title: req.body.title,
+    album: req.body.album,
+    duration: req.body.duration,
+  }
+
+  const track = new Track(newTrack);
+
+  try {
+    await track.save();
+    return res.send(track);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e)
+    } else {
+      return next(e)
+    }
+  }
 });
 
 export default tracksRouter
