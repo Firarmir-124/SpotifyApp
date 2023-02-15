dfd20c33ab18ae3efbf5008aae76b7481f2774f0
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-15 13:36:05 +0600

    Написал роутер post запроса sessions

--- routes/users.ts
+++ routes/users.ts
@@ -22,4 +22,20 @@ usersRouter.post('/', async (req, res, next) => {
   }
 });
 
+usersRouter.post('/sessions', async (req, res) => {
+  const user = await User.findOne({username: req.body.username});
+
+  if (!user) {
+    return res.status(404).send({error: 'User not found'});
+  }
+
+  const isMatch = await user.checkPassword(req.body.password);
+
+  if (!isMatch) {
+    return res.status(404).send({error: 'Password not found'});
+  }
+
+  return res.send({message: 'Username and password correct !'})
+})
+
 export default usersRouter;
