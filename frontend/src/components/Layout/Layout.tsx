09a316a18cc8ddd170e5c38b42bebd2e86910d66
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-22 17:19:03 +0600

    Создал компонент Layout и сдалал верстку

---
+++ frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Layout:React.FC<Props> = ({children}) => {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Spotify
+          </Typography>
+          <Button component={Link} to='/register' color="inherit">Sign up</Button>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {children}
+      </main>
+    </>
+  );
+};
+
+export default Layout;
