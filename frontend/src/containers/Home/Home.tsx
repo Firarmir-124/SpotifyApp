f4ae31c42e2179ff6817231790bdec4eef70aa92
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-22 20:25:10 +0600

    Описал логику получения исполнителей в компонентах

--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from "../../components/Layout/Layout";
-import {Card, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
+import {CircularProgress, Container, Grid, LinearProgress, Typography} from "@mui/material";
 import CartExecutor from "../../components/CartExecutor/CartExecutor";
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import {selectExecutorLoading, selectExecutors} from "../../store/executorSlice";
+import {fetchExecutor} from "../../store/executorThunk";
 
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const executors = useAppSelector(selectExecutors);
+  const loading = useAppSelector(selectExecutorLoading);
+
+  useEffect(() => {
+    dispatch(fetchExecutor());
+  }, [dispatch]);
+
   return (
     <Layout>
       <Container sx={{mt: 5}}>
@@ -13,9 +24,13 @@ const Home = () => {
         </Typography>
 
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {Array.from(Array(10)).map((_, index) => (
-            <CartExecutor/>
-          ))}
+          {
+            !loading ? (
+              executors.map((executor) => (
+                <CartExecutor key={executor._id} executor={executor}/>
+              ))
+            ) : <CircularProgress sx={{m: 5}}/>
+          }
         </Grid>
       </Container>
     </Layout>
