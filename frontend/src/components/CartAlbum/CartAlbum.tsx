5ab09ada214ad959cfdcf8e14072102b963dba6e
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-22 19:18:08 +0600

    Изменил url у ссылки

--- frontend/src/components/CartAlbum/CartAlbum.tsx
+++ frontend/src/components/CartAlbum/CartAlbum.tsx
@@ -6,7 +6,7 @@ const CartAlbum = () => {
   return (
     <Grid item xs={2} sm={4} md={4}>
       <Card variant="outlined" sx={{ maxWidth: 400 }}>
-        <Link to={'albums/' + 'id'}>
+        <Link to={'/albums/' + 'id'}>
           <CardMedia
             sx={{ height: 200 }}
             image="https://static-cse.canva.com/blob/969343/1600w-gLXlb7kYql0.jpg"
