e5c0bace6e7ccf2fa69259a84f624157ec7ad6aa
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-22 20:03:36 +0600

    Описал логику получения всех артистов в slice

--- frontend/src/store/executorSlice.ts
+++ frontend/src/store/executorSlice.ts
@@ -1,5 +1,7 @@
 import {Artists} from "../types";
 import {createSlice} from "@reduxjs/toolkit";
+import {fetchExecutor} from "./executorThunk";
+import {RootState} from "../app/store";
 
 interface executorType {
   executors: Artists[];
@@ -14,7 +16,21 @@ const initialState:executorType = {
 export const executorSlice = createSlice({
   name: 'executor',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchExecutor.pending, (state) => {
+      state.executorLoading = true;
+    });
+    builder.addCase(fetchExecutor.fulfilled, (state, {payload: executor}) => {
+      state.executorLoading = false;
+      state.executors = executor;
+    });
+    builder.addCase(fetchExecutor.rejected, (state) => {
+      state.executorLoading = false;
+    });
+  }
 });
 
 export const executorReducer = executorSlice.reducer;
+export const selectExecutors = (state: RootState) => state.executor.executors;
+export const selectExecutorLoading = (state: RootState) => state.executor.executorLoading;
