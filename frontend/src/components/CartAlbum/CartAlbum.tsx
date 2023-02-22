b2c6a92cdc90da8e4ea051b46e1a679ee6b915de
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-22 18:07:38 +0600

    Создал компонент CartAlbum и подлкючил к Albums

---
+++ frontend/src/components/CartAlbum/CartAlbum.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Card, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
+
+const CartAlbum = () => {
+  return (
+    <Grid item xs={2} sm={4} md={4}>
+      <Card variant="outlined" sx={{ maxWidth: 400 }}>
+        <Link to={'albums/' + 'id'}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="https://static-cse.canva.com/blob/969343/1600w-gLXlb7kYql0.jpg"
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography component="p">
+              Название альбома
+            </Typography>
+            <Typography component="p">
+              Год выхода
+            </Typography>
+            <Typography component="p">
+              Количество треков
+            </Typography>
+          </CardContent>
+        </Link>
+      </Card>
+    </Grid>
+  );
+};
+
+export default CartAlbum;
--- frontend/src/containers/Albums/Albums.tsx
+++ frontend/src/containers/Albums/Albums.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from "../../components/Layout/Layout";
 import {Card, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
-import {Link} from "react-router-dom";
+import CartAlbum from "../../components/CartAlbum/CartAlbum";
 
 const Albums = () => {
   return (
@@ -13,28 +13,7 @@ const Albums = () => {
 
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           {Array.from(Array(10)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4}>
-              <Card variant="outlined" sx={{ maxWidth: 400 }}>
-                <Link to={'albums/' + 'id'}>
-                  <CardMedia
-                    sx={{ height: 200 }}
-                    image="https://static-cse.canva.com/blob/969343/1600w-gLXlb7kYql0.jpg"
-                    title="green iguana"
-                  />
-                  <CardContent>
-                    <Typography component="p">
-                      Название альбома
-                    </Typography>
-                    <Typography component="p">
-                      Год выхода
-                    </Typography>
-                    <Typography component="p">
-                      Количество треков
-                    </Typography>
-                  </CardContent>
-                </Link>
-              </Card>
-            </Grid>
+            <CartAlbum/>
           ))}
         </Grid>
       </Container>
