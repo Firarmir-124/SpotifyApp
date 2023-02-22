f8a12ae66582d6de23cc92e4c7bd1f9e8d2d3dfc
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-22 18:04:33 +0600

    Добавил ссылку к альбомом

--- frontend/src/containers/Albums/Albums.tsx
+++ frontend/src/containers/Albums/Albums.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from "../../components/Layout/Layout";
 import {Card, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
 
 const Albums = () => {
   return (
@@ -14,22 +15,24 @@ const Albums = () => {
           {Array.from(Array(10)).map((_, index) => (
             <Grid item xs={2} sm={4} md={4}>
               <Card variant="outlined" sx={{ maxWidth: 400 }}>
-                <CardMedia
-                  sx={{ height: 200 }}
-                  image="https://static-cse.canva.com/blob/969343/1600w-gLXlb7kYql0.jpg"
-                  title="green iguana"
-                />
-                <CardContent>
-                  <Typography component="p">
-                    Название альбома
-                  </Typography>
-                  <Typography component="p">
-                    Год выхода
-                  </Typography>
-                  <Typography component="p">
-                    Количество треков
-                  </Typography>
-                </CardContent>
+                <Link to={'albums/' + 'id'}>
+                  <CardMedia
+                    sx={{ height: 200 }}
+                    image="https://static-cse.canva.com/blob/969343/1600w-gLXlb7kYql0.jpg"
+                    title="green iguana"
+                  />
+                  <CardContent>
+                    <Typography component="p">
+                      Название альбома
+                    </Typography>
+                    <Typography component="p">
+                      Год выхода
+                    </Typography>
+                    <Typography component="p">
+                      Количество треков
+                    </Typography>
+                  </CardContent>
+                </Link>
               </Card>
             </Grid>
           ))}
