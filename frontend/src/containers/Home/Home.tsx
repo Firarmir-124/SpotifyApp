cc5fda869a505150162e04c21da5fc22a595b463
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-24 13:41:10 +0600

    Изменил верстку в контейнере Home

--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import Layout from "../../components/Layout/Layout";
-import {CircularProgress, Container, Grid, LinearProgress, Typography} from "@mui/material";
+import {Chip, CircularProgress, Container, Grid, LinearProgress, Paper, Typography} from "@mui/material";
 import CartExecutor from "../../components/CartExecutor/CartExecutor";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {selectExecutorLoading, selectExecutors} from "../../store/executorSlice";
@@ -17,21 +17,25 @@ const Home = () => {
 
   return (
     <Layout>
-      <Container sx={{mt: 5}}>
+      <Container>
+        <Chip
+          sx={{my: 2, fontSize: '20px', p: 3}}
+          label="Исполнители"
+          variant="outlined"
+          color="info"
+        />
 
-        <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
-          Исполнители
-        </Typography>
-
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {
-            !loading ? (
-              executors.map((executor) => (
-                <CartExecutor key={executor._id} executor={executor}/>
-              ))
-            ) : <CircularProgress sx={{m: 5}}/>
-          }
-        </Grid>
+        <Paper elevation={3} sx={{minHeight: '80vh', p: 1}}>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {
+              !loading ? (
+                executors.map((executor) => (
+                  <CartExecutor key={executor._id} executor={executor}/>
+                ))
+              ) : <CircularProgress sx={{m: 5}}/>
+            }
+          </Grid>
+        </Paper>
       </Container>
     </Layout>
   );
