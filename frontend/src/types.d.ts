5e14b18da8b00a4ab19f59acd312d27eb182e7e1
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-25 18:54:58 +0600

    HW#86 Добавил новые типы в frontend

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -36,3 +36,51 @@ export interface Artists extends ArtistMutation{
 export interface Tracks extends TrackMutation{
   _id: string;
 }
+
+export interface RegisterMutation {
+  username: string;
+  password: string;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  message: string;
+  user: User;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    }
+  },
+  name: string;
+  _name: string;
+}
+
+export interface LoginMutation {
+  username: string;
+  password: string;
+}
+
+export interface GlobalError {
+  error: string;
+}
+
+export interface TracksHistory {
+  _id: string;
+  user: string;
+  track: Tracks;
+  executor: Artists;
+  datetime: string;
+}
+
+export interface TracksHistoryMutation {
+  track: string;
+}
