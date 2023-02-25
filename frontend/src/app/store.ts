798cff5265735cdf85c19f864cf86b7764446993
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-25 21:28:18 +0600

    HW#86 Настроил redux-persist в store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,13 +1,32 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {executorReducer} from "../store/executorSlice";
 import {userReducer} from "../store/userSlice";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants";
+
+const userPersistConfig = {
+  key: 'spotify:users',
+  storage,
+  whitelist: ['user'],
+}
+
+const rootReducer = combineReducers({
+  executor: executorReducer,
+  users: persistReducer(userPersistConfig, userReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    executor: executorReducer,
-    users: userReducer,
-  }
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
