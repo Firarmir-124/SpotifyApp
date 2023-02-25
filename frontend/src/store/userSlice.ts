3f3a278a9b24a97abff53d309b8b15361ba737e7
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-25 19:57:13 +0600

    HW#86 Описал логику регистрации пользователя в slice

--- frontend/src/store/userSlice.ts
+++ frontend/src/store/userSlice.ts
@@ -1,5 +1,7 @@
 import {User, ValidationError} from "../types";
 import {createSlice} from "@reduxjs/toolkit";
+import {register} from "./userThunk";
+import {RootState} from "../app/store";
 
 interface UserType {
   user: User | null;
@@ -17,6 +19,23 @@ export const userSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(register.pending, (state) => {
+      state.registerLoading = true;
+      state.registerError = null
+    });
+    builder.addCase(register.fulfilled, (state, {payload: user}) => {
+      state.registerLoading = false;
+      state.user = user
+    });
+    builder.addCase(register.rejected, (state, {payload: error}) => {
+      state.registerLoading = false;
+      state.registerError = error || null
+    });
+  }
 });
 
 export const userReducer = userSlice.reducer;
+export const selectUser = (state: RootState) => state.users.user;
+export const selectRegisterError = (state: RootState) => state.users.registerError;
+export const selectRegisterLoading = (state: RootState) => state.users.registerLoading;
