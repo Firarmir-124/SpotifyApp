6b062622be51a6fad8c21c33f015d0da56594104
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-25 16:27:49 +0600

    HW#86 Переделал роутер trackHistory

--- backend/routes/trackHistory.ts
+++ backend/routes/trackHistory.ts
@@ -2,30 +2,40 @@ import express from "express";
 import TrackHistory from "../models/TrackHistory";
 import {Error} from "mongoose";
 import auth, {RequestWitUser} from "../middleware/auth";
+import Track from "../models/Track";
+import {TrackPopulate} from "../types";
 
 const trackHistoryRouter = express.Router();
+
 
 trackHistoryRouter.post('/', auth, async (req, res, next) => {
   const user = (req as RequestWitUser).user;
 
-  const newHistory = {
-    user: user._id,
-    track: req.body.track,
-    datetime: (new Date()).toISOString(),
-  };
-
-  const history = new TrackHistory(newHistory);
-
   try {
-    await history.save();
-    return res.send(history);
+    const executorId:TrackPopulate | null = await Track.findOne({_id: req.body.track}).populate('album');
 
-  }catch (e) {
-    if (e instanceof Error.ValidationError) {
-      return res.status(404).send(e);
-    } else {
-      return next(e)
+    const newHistory = {
+      user: user._id,
+      track: req.body.track,
+      executor: executorId && executorId.album.executor,
+      datetime: (new Date()).toISOString(),
+    };
+
+    const history = new TrackHistory(newHistory);
+
+    try {
+      await history.save();
+      return res.send(history);
+
+    }catch (e) {
+      if (e instanceof Error.ValidationError) {
+        return res.status(404).send(e);
+      } else {
+        return next(e)
+      }
     }
+  }catch {
+    return res.sendStatus(500);
   }
 });
 
