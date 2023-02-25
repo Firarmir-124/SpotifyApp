08eb386f4543bc45e83029b2dc1447aab2a3ae88
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-25 21:07:04 +0600

    HW#86 Создал компонент AnonymousMenu

---
+++ frontend/src/components/Layout/AnonymousMenu/AnonymousMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button} from "@mui/material";
+import {Link} from "react-router-dom";
+
+const Anonymous = () => {
+  return (
+    <>
+      <Button component={Link} to='/register' color="inherit">Register</Button>
+      <Button component={Link} to='/login' color="inherit">Login</Button>
+    </>
+  );
+};
+
+export default Anonymous;
