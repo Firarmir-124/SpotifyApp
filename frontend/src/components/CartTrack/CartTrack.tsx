29153be0f165d8b4c59e45ee041670c7f26e574d
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-26 23:23:40 +0600

    HW#86 Переделал верстку в компоненте CartTrack и добавил картинку в проект

--- frontend/src/components/CartTrack/CartTrack.tsx
+++ frontend/src/components/CartTrack/CartTrack.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import {Box, Card, CardContent, CardMedia, Chip, Grid, Typography} from "@mui/material";
+import {Box, Card, CardContent, CardMedia, Chip, Grid, IconButton, Typography} from "@mui/material";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import {Tracks} from "../../types";
+import music from '../../assets/images/music.png';
+
 interface Props {
   track: Tracks
 }
 
 const CartTrack:React.FC<Props> = ({track}) => {
   return (
-    <Grid item xs={2} sm={4} md={4}>
-      <Card sx={{ display: 'flex' }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardContent sx={{ flex: '1 0 auto' }}>
-            <Typography component="div" variant="h5">
-              {track.title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              {track.duration}
-            </Typography>
-            <Chip size='small' label={track.trackNumber} color="primary" />
-          </CardContent>
-        </Box>
-        <CardMedia
-          component="img"
-          sx={{ width: 150 }}
-          image="https://mui.com/static/images/cards/live-from-space.jpg"
-          alt="Live from space album cover"
-        />
-      </Card>
-    </Grid>
+    <Card
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        height: '100px',
+        mb: 1
+      }}
+      elevation={4}
+    >
+      <Box component='div'>
+        <Chip variant='outlined' sx={{ml: 1}} size='small' label={track.trackNumber} color="primary" />
+        <IconButton aria-label="play/pause">
+          <PlayArrowIcon sx={{ height: 40, width: 40 }} />
+        </IconButton>
+      </Box>
+      <CardContent sx={{display: 'flex',  justifyContent: 'space-between', alignItems: 'center', width: '50%'}}>
+        <Typography component="div" variant="h6">
+          {track.title}
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary" component="div">
+          {track.duration}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        component="img"
+        sx={{ width: 80 }}
+        image={music}
+        alt="Live from space album cover"
+      />
+    </Card>
   );
 };
 
