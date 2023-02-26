15129316f5f6af89da710d35bb975387026470ab
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-26 23:45:15 +0600

    HW#86 Описал логику получения историю треков в slice

--- frontend/src/store/trackHistorySlice.ts
+++ frontend/src/store/trackHistorySlice.ts
@@ -1,13 +1,18 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {trackHistoryPost} from "./executorThunk";
+import {trackHistoryGet, trackHistoryPost} from "./executorThunk";
 import {RootState} from "../app/store";
+import {TracksHistory} from "../types";
 
 interface trackHistoryType {
   trackHistoryPostLoading: boolean;
+  trackHistory: TracksHistory[];
+  trackHistoryGetLoading: boolean;
 }
 
 const initialState:trackHistoryType = {
   trackHistoryPostLoading: false,
+  trackHistory: [],
+  trackHistoryGetLoading: false,
 };
 
 export const trackHistorySlice = createSlice({
@@ -24,8 +29,21 @@ export const trackHistorySlice = createSlice({
     builder.addCase(trackHistoryPost.rejected, (state) => {
       state.trackHistoryPostLoading = false;
     });
+
+    builder.addCase(trackHistoryGet.pending, (state) => {
+      state.trackHistoryGetLoading = true;
+    });
+    builder.addCase(trackHistoryGet.fulfilled, (state, {payload: trackHistory}) => {
+      state.trackHistoryGetLoading = false;
+      state.trackHistory = trackHistory;
+    });
+    builder.addCase(trackHistoryGet.rejected, (state) => {
+      state.trackHistoryGetLoading = false;
+    });
   }
 });
 
 export const trackHistoryReducer = trackHistorySlice.reducer;
 export const trackHistoryPostLoading = (state: RootState) => state.trackHistory.trackHistoryPostLoading;
+export const selectTrackHistory = (state: RootState) => state.trackHistory.trackHistory;
+export const selectTrackHistoryGetLoading = (state: RootState) => state.trackHistory.trackHistoryGetLoading;
