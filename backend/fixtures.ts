f22b5e2eea5d6cf2fc7d153fde7006a580bf9f28
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-27 15:28:53 +0600

    HW#86 Переделал фикстуры

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -63,77 +63,77 @@ const run = async () => {
       title: 'Я жду',
       album: album1._id,
       duration: '5 минут 2 секунды',
-      youtubeLink: "https://youtu.be/F1j6BBdWJqc",
+      youtubeLink: "F1j6BBdWJqc",
       trackNumber: 1
     },
     {
       title: 'История любви',
       album: album1._id,
       duration: '3 минут 57 секунды',
-      youtubeLink: "https://youtu.be/AKOTCO8CujY",
+      youtubeLink: "AKOTCO8CujY",
       trackNumber: 2
     },
     {
       title: 'Love Story (Club Mix)',
       album: album1._id,
       duration: '5 минут 2 секунды',
-      youtubeLink: "https://youtu.be/CGDHP7yhsm4",
+      youtubeLink: "CGDHP7yhsm4",
       trackNumber: 3
     },
     {
       title: 'Он тебя целует',
       album: album1._id,
       duration: '3 минут 32 секунды',
-      youtubeLink: "https://youtu.be/phxQFEH51SE",
+      youtubeLink: "phxQFEH51SE",
       trackNumber: 4
     },
     {
       title: 'Здравствуй',
         album: album1._id,
       duration: '4 минут 55 секунды',
-      youtubeLink: "https://youtu.be/h7CQC5lmppc",
+      youtubeLink: "h7CQC5lmppc",
       trackNumber: 5
     },
     {
       title: 'Малыш',
       album: album2._id,
       duration: '3 минут 50 секунды',
-      youtubeLink: "https://youtu.be/MuWQZyXrvaA",
+      youtubeLink: "MuWQZyXrvaA",
       trackNumber: 1
     },
     {
       title: 'Последний поцелуй',
       album: album2._id,
       duration: '3 минут 53 секунды',
-      youtubeLink: "https://youtu.be/2gBvOiOxCOg",
+      youtubeLink: "2gBvOiOxCOg",
       trackNumber: 2
     },
     {
       title: 'Бандито',
       album: album2._id,
       duration: '2 минут 33 секунды',
-      youtubeLink: "https://youtu.be/pEM5o5kR1ms",
+      youtubeLink: "pEM5o5kR1ms",
       trackNumber: 3
     },
     {
       title: 'По кайфу',
       album: album2._id,
       duration: '3 минут 17 секунды',
-      youtubeLink: "https://youtu.be/DVyacTfUo7E",
+      youtubeLink: "DVyacTfUo7E",
       trackNumber: 4
     },
     {
       title: '1, 2, 3, 4, 5 (Gin Mix)',
       album: album2._id,
       duration: '5 минут 40 секунды',
-      youtubeLink: "https://youtu.be/Uw0nID45Hbw",
+      youtubeLink: "Uw0nID45Hbw",
       trackNumber: 5
     },
     {
       title: 'Лали',
       album: album3._id,
       duration: '2 минут 28 секунды',
-      youtubeLink: "https://youtu.be/SucUYI6Vnj0",
+      youtubeLink: "SucUYI6Vnj0",
       trackNumber: 1
     },
     {
@@ -147,56 +147,56 @@ const run = async () => {
       title: 'Комета',
       album: album3._id,
       duration: '2 минут 39 секунды',
-      youtubeLink: "https://youtu.be/yM1QjdoLmxQ",
+      youtubeLink: "yM1QjdoLmxQ",
       trackNumber: 3
     },
     {
       title: 'Титры',
       album: album3._id,
       duration: '3 минут 22 секунды',
-      youtubeLink: "https://youtu.be/9Jr2dCEMxis",
+      youtubeLink: "9Jr2dCEMxis",
       trackNumber: 4
     },
     {
       title: 'Босс',
       album: album3._id,
       duration: '3 минут 14 секунды',
-      youtubeLink: "https://youtu.be/pJBgdXUwp28",
+      youtubeLink: "pJBgdXUwp28",
       trackNumber: 5
     },
     {
       title: 'Аллея',
       album: album4._id,
       duration: '2 минут 25 секунды',
-      youtubeLink: "https://youtu.be/5R6BYT176Bk",
+      youtubeLink: "5R6BYT176Bk",
       trackNumber: 1
     },
     {
       title: 'Без тебя я не я',
       album: album4._id,
       duration: '3 минут 20 секунды',
-      youtubeLink: "https://youtu.be/xtDQF0J6NjY",
+      youtubeLink: "xtDQF0J6NjY",
       trackNumber: 2
     },
     {
       title: 'Наверно, ты меня не помнишь',
       album: album4._id,
       duration: '3 минут 07 секунды',
-      youtubeLink: "https://youtu.be/Sh8Qz0JtVuY",
+      youtubeLink: "Sh8Qz0JtVuY",
       trackNumber: 3
     },
     {
       title: 'Ты пари',
       album: album4._id,
       duration: '3 минут 55 секунды',
-      youtubeLink: "https://youtu.be/pRp5RYkVyas",
+      youtubeLink: "pRp5RYkVyas",
       trackNumber: 4
     },
     {
       title: 'Никак',
       album: album4._id,
       duration: '4 минут 23 секунды',
-      youtubeLink: "https://youtu.be/Lg2BJu8NHOU",
+      youtubeLink: "Lg2BJu8NHOU",
       trackNumber: 5
     },
   );
