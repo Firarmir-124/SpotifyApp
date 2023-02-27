af5ad36fa730266ccaca05be155864c2c93a7308
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-27 15:39:46 +0600

    HW#86 Написал логику прослушивание песни в youtube

--- frontend/src/containers/Tracks/Tracks.tsx
+++ frontend/src/containers/Tracks/Tracks.tsx
@@ -1,14 +1,27 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Layout from "../../components/Layout/Layout";
-import {Alert, Chip, CircularProgress, Container, Paper} from "@mui/material";
+import {
+  Alert,
+  Button,
+  Chip,
+  CircularProgress,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Paper
+} from "@mui/material";
 import {Navigate, useParams} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {selectAlbum, selectAlbumOneLoading, selectTracks, selectTracksLoading} from "../../store/executorSlice";
 import {fetchAlbum, fetchTracks, trackHistoryPost} from "../../store/executorThunk";
 import CartTrack from "../../components/CartTrack/CartTrack";
 import {selectUser} from "../../store/userSlice";
+import YouTube, {YouTubeProps} from "react-youtube";
+import {getVideoId, selectVideoId} from "../../store/trackHistorySlice";
 
 const Tracks = () => {
+  const [open, setOpen] = useState(false);
   const {id} = useParams();
   const dispatch = useAppDispatch();
   const album = useAppSelector(selectAlbum);
@@ -16,6 +29,7 @@ const Tracks = () => {
   const tracks = useAppSelector(selectTracks);
   const loadingTrack = useAppSelector(selectTracksLoading);
   const user = useAppSelector(selectUser);
+  const videoId = useAppSelector(selectVideoId);
 
   const getInformation = useCallback(async () => {
     if (id) {
@@ -28,8 +42,18 @@ const Tracks = () => {
     void getInformation();
   }, [getInformation]);
 
-  const trackHistory = async (id: string) => {
+  const trackHistory = async (id: string, videoId: string) => {
     await dispatch(trackHistoryPost(id));
+    dispatch(getVideoId(videoId));
+    setOpen(true);
+  };
+
+  const opts: YouTubeProps['opts'] = {
+    height: '400',
+    width: '400',
+    playerVars: {
+      autoplay: 1,
+    },
   };
 
   if (!user) {
@@ -54,12 +78,27 @@ const Tracks = () => {
             !loadingTrack ? (
               tracks.length !== 0 ? (
                 tracks.map((track) => (
-                  <CartTrack trackHistory={() => trackHistory(track._id)} key={track._id} track={track}/>
+                  <CartTrack trackHistory={() => trackHistory(track._id, track.youtubeLink)} key={track._id} track={track}/>
                 ))
               ) : <Alert severity="info">У альбома нет песен !</Alert>
             ) : <CircularProgress/>
           }
         </Paper>
+
+
+        <Dialog
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogContent>
+            <YouTube videoId={videoId} opts={opts}/>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+          </DialogActions>
+        </Dialog>
       </Container>
     </Layout>
   );
