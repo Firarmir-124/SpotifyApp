e80887d9fef3e67733b5c8213aae4da4e7d38761
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-09 17:43:19 +0600

    HW#89 Добавил новую настройку для axios и подключил к index

--- frontend/src/axiosApi.ts
+++ frontend/src/axiosApi.ts
@@ -1,8 +1,20 @@
-import axios from "axios";
+import axios, {AxiosHeaders, AxiosRequestConfig} from "axios";
 import {apiURl} from "./constans";
+import {Store} from "@reduxjs/toolkit";
+import {RootState} from "./app/store";
 
 const axiosApi = axios.create({
   baseURL: apiURl
 });
+
+export const addInterceptors = (store: Store<RootState>) => {
+  axiosApi.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = store.getState().users.user?.token;
+    const headers = config.headers as AxiosHeaders;
+    headers.set('Authorization', token);
+
+    return config;
+  });
+};
 
 export default axiosApi;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,16 +6,19 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {persistor, store} from "./app/store";
 import {PersistGate} from "redux-persist/integration/react";
+import {addInterceptors} from "./axiosApi";
+
+addInterceptors(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </Provider>
+      </BrowserRouter>
     </PersistGate>
-  </BrowserRouter>
+  </Provider>
 );
